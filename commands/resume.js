a98368a46e9e2a011c9f81cadefeccc0ce7b1a06
David House <david.house@kp.org>
2019-09-24 21:22:09 -0400

    :sparkles: Pause and resume commands

---
+++ commands/resume.js
@@ -0,0 +1,17 @@
+const Queue = require('bull')
+const chalk = require('chalk')
+
+/**
+ * resume command
+ * @param {*} name
+ * @param {*} redisConfig
+ * @param {*} callback
+ */
+async function handle(name, redisConfig, callback) {
+  const q = new Queue('stampede-' + name, redisConfig)
+  q.resume()
+  console.log(chalk.green(name + ' resumed'))
+  callback()
+}
+
+module.exports.handle = handle
