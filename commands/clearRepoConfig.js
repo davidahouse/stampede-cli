9eb921c583638bc1190ac153388c41a1e360d251
David House <davidahouse@gmail.com>
2019-10-16 13:06:35 -0400

    :sparkles: added clearRepoConfig

---
+++ commands/clearRepoConfig.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const yaml = require('js-yaml')
+
+/**
+ * clearRepoConfig command
+ * @param {*} owner
+ * @param {*} repo
+ * @param {*} file
+ * @param {*} cache
+ * @param {*} callback
+ */
+async function handle(owner, repo, file, cache, callback) {
+  cache.removeRepoConfig(owner, repo)
+  callback()
+}
+
+module.exports.handle = handle
